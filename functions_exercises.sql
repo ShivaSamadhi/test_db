0f5c38ef6ce63e6d3933a63954fe6aaf646ec1f9
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-26 15:28:41 -0500

    functions exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,48 @@
+Use employees;
+
+Select * From employees
+Where gender = 'M'
+  And (
+            first_name = 'Irena'
+        Or first_name = 'Vidya'
+        Or first_name = 'Maya'
+    )
+Order By last_name, first_name;
+
+Select emp_no, first_name, last_name
+From employees
+Where last_name LIKE 'E%'
+Order By emp_no DESC;
+
+Select first_name, last_name
+From employees
+Where last_name LIKE '%Q%';
+
+Select emp_no, first_name, last_name
+From employees
+Where last_name LIKE 'E%'
+   Or last_name LIKE '%E'
+Order By emp_no DESC;
+
+Select emp_no, Concat(first_name, ' ', last_name)
+From employees
+Where last_name LIKE 'E%'
+  And last_name LIKE '%E'
+Order By emp_no DESC;
+
+Select first_name, last_name
+From employees
+Where last_name Like '%Q%'
+  And last_name Not Like '%qu%';
+
+Select Concat(first_name, ' ', last_name)
+From employees
+Where month(birth_date) = 12
+  AND day(birth_date) = 25;
+
+Select Concat(first_name, ' ', last_name, ' ', hire_date)
+From employees
+Where year(hire_date) Between 1990 And 1999
+  And month(birth_date) = 12
+  AND day(birth_date) = 25
+Order By year(hire_date) Desc;
