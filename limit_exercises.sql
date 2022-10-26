526bb31b60326433f34cea37cd770ed3134ae776
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-26 13:41:13 -0500

    limits exercises 3

---
+++ limit_exercises.sql
@@ -0,0 +1,12 @@
+Use employees;
+
+Select Distinct last_name
+From employees
+Where last_name Like 'Z%'
+Order By last_name DESC
+Limit 10;
+
+Select emp_no
+From salaries
+Order By salary Desc
+Limit 5;
