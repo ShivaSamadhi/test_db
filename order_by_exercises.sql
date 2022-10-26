fbdd020f2441ef516a272b989ff5d0fbe44b171f
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-26 12:28:46 -0500

    order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,35 +1,34 @@
 Use employees;
 
 Select * From employees
-Where first_name In ('Irena', 'Vidya', 'Maya')
-Order By first_name;
-
-Select first_name, last_name
-From employees
-Where last_name LIKE 'E%';
-
-Select first_name, last_name
-From employees
-Where last_name LIKE '%Q%';
-
-Select * From employees
 Where gender = 'M'
   And (
             first_name = 'Irena'
         Or first_name = 'Vidya'
         Or first_name = 'Maya'
     )
-Order By first_name;;
+Order By last_name, first_name;
 
 Select first_name, last_name
 From employees
 Where last_name LIKE 'E%'
-   Or last_name LIKE '%E';
+Order By emp_no;
+
+Select first_name, last_name
+From employees
+Where last_name LIKE '%Q%';
 
 Select first_name, last_name
 From employees
 Where last_name LIKE 'E%'
-  And last_name LIKE '%E';
+   Or last_name LIKE '%E'
+Order By emp_no;
+
+Select first_name, last_name
+From employees
+Where last_name LIKE 'E%'
+  And last_name LIKE '%E'
+Order By emp_no;
 
 Select first_name, last_name
 From employees
