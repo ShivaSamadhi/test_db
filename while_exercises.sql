52617d1eaf140f933651f3256f8cd8a3b27b9aa1
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-26 12:08:45 -0500

    where exercises part 2

--- while_exercises.sql
+++ while_exercises.sql
@@ -3,8 +3,33 @@ Use employees;
 Select * From employees
 Where first_name In ('Irena', 'Vidya', 'Maya');
 
-Select first_name, last_name From employees
+Select first_name, last_name
+From employees
 Where last_name LIKE 'E%';
 
-Select first_name, last_name From employees
+Select first_name, last_name
+From employees
 Where last_name LIKE '%Q%';
+
+Select * From employees
+Where gender = 'M'
+  And (
+        first_name = 'Irena'
+        Or first_name = 'Vidya'
+        Or first_name = 'Maya'
+    );
+
+Select first_name, last_name
+From employees
+Where last_name LIKE 'E%'
+   Or last_name LIKE '%E';
+
+Select first_name, last_name
+From employees
+Where last_name LIKE 'E%'
+   And last_name LIKE '%E';
+
+Select first_name, last_name
+From employees
+Where last_name Like '%Q%'
+  And last_name Not Like '%qu%';
